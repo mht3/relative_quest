47ea306b30e0d989524c79c69a3661fcd168de15
ArjunBadami <arjunbadami777@gmail.com>
2024-03-04 23:50:34 -0800

    args update for cli

--- codes/relative_quest.py
+++ codes/relative_quest.py
@@ -30,39 +30,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-'''
-from typing import Optional, List
-
-import typer
-
-from __init__ import __app_name__, __version__
-from codes import ersa
-app = typer.Typer()
-
-def _version_callback(value: bool) -> None:
-    if value:
-        typer.echo(f"{__app_name__} v{__version__}")
-        raise typer.Exit()
-
-@app.callback()
-def main(
-    version: Optional[bool] = typer.Option(
-        None,
-        "--version",
-        "-v",
-        help="Show the application's version and exit.",
-        callback=_version_callback,
-        is_eager=True,
-    )
-) -> None:
-    return
-
-@app.command()
-def findrelatives(
-        matchfile: List[str] = typer.Argument(...),
-        threshold: int = typer.Option(2, "--threshold", "-t", min=1, max=3),
-) -> None:
-    match_file = 'data/test/germline/expected.match'
-    e = ersa.ERSA(match_file, threshold=2.5)
-    typer.secho('SUCCESS! Generated file:')
-'''
