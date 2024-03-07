3781f9c1d13730a9cc056f6a6c03956451a62e81
mht3 <mht3@illinois.edu>
2024-03-06 23:03:25 -0800

    added thresholded predictions for plink ibd

--- codes/ibd_results.py
+++ codes/ibd_results.py
@@ -27,10 +27,56 @@ def plot_ibd(ibd_df):
   plt.tight_layout()
   plt.show()
 
+def predict_plink_results(ibd):
+  relationships = {}
+  lax_threshold = 0.1
+  threshold = 0.05
+  for i, row in ibd.iterrows():
+      z0 = row['Z0']
+      z1 = row['Z1']
+      z2 = row['Z2']
+      fid1 = row['FID1']
+      fid2 = row['FID2']
+      # parent child relationships
+      if (z1 > 1. - lax_threshold):
+        if relationships.get('Parent-child', -1) == -1:
+          relationships['Parent-child'] = []
+        relationships['Parent-child'].append([fid1, fid2])
+      elif (z2 > 0.25 - threshold and z1 > 0.5 - lax_threshold):
+        # siblings requirement
+        if relationships.get('Siblings', -1) == -1:
+          relationships['Siblings'] = []
+        relationships['Siblings'].append([fid1, fid2])
+      elif (z2 > 1. - lax_threshold):
+        # Identical twins requirement
+        if relationships.get('Identical Twins', -1) == -1:
+          relationships['Identical Twins'] = []
+        relationships['Identical Twins'].append([fid1, fid2])
+      elif (z1 > 0.5 - 1.5*lax_threshold and z0 > 0.5 - 1.5*lax_threshold):
+        if relationships.get('Second Order', -1) == -1:
+          relationships['Second Order'] = []
+        relationships['Second Order'].append([fid1, fid2])
+      elif (z1 > 0.25 - lax_threshold and z0 > 0.75 - lax_threshold):
+        if relationships.get('First Cousins', -1) == -1:
+          relationships['First Cousins'] = []
+        relationships['First Cousins'].append([fid1, fid2])
+      elif (z1 > 0.0625):
+        if relationships.get('Second Cousins', -1) == -1:
+          relationships['Second Cousins'] = []
+        relationships['Second Cousins'].append([fid1, fid2])
+      elif (z1 > 1 / (2**6)):
+        if relationships.get('Third Cousins', -1) == -1:
+          relationships['Third Cousins'] = []
+        relationships['Third Cousins'].append([fid1, fid2])
+  return relationships
+
 if __name__ == '__main__':
     # example on how to plot a plink.genome output file
     filename = "data/test/plink/lwk.ibd.genome"
-    ibd = pd.read_csv(filename, delim_whitespace=True)
+    print('##### Plink #####')
+    ibd = pd.read_csv(filename, sep='\s+')
+    relationships = predict_plink_results(ibd)
+    print({k:len(v) for k, v in relationships.items()})
     plot_ibd(ibd_df=ibd)
 
     # example how to run and plot ersa.genome output file
